e92f716ae6ec30d5c94c32115234daca3ce4e9af
Andrija Jelenkovic <andirja.jelenkovic@protonmail.com>
2022-07-01 19:03:25 +0200

    Add domain project

---
+++ BudgetApp.Domain/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace BudgetApp.Domain
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; init; }
+
+        public string Color { get; init; }
+
+        public string UserId { get; init; }
+
+        public Category()
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = "";
+            this.Color = "#FFFFFF";
+            this.UserId = "";
+        }
+    }
+}
