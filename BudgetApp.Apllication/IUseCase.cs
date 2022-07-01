4c34e350eb5185e76585d1ed65ba756906e8926e
Andrija Jelenkovic <andirja.jelenkovic@protonmail.com>
2022-07-01 19:03:35 +0200

    Add application project

---
+++ BudgetApp.Apllication/IUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetApp.Apllication
+{
+    public interface IUseCase<TRequest, TResponse>
+    {
+        public Task<TResponse> ExecuteAsync(TRequest request);
+    }
+}
